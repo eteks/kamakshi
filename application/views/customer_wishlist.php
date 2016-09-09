9ea04a2a2dfa0758481e973d8c3118a90fb623d0
velpandip <pvvelpandi@gmail.com>
2016-09-09 20:11:26 +0530

    After changing in footer content.

--- application/views/customer_wishlist.php
+++ application/views/customer_wishlist.php
@@ -772,7 +772,7 @@
         <div id="copyright">
             <div class="container">
                 <div class="col-md-6">
-                    <p class="pull-left">© 2015 Your name goes here.</p>
+                    <p class="pull-left">© 2016 Kamakshi.</p>
 
                 </div>
                 <div class="col-md-6">
