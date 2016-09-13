ce5b166ab0deb00717e0ddc3859031a13845dcf1
kalaiR <kalamca.gs@gmail.com>
2016-09-13 16:48:11 +0530

    update admin header part

--- application/views/admin/templates/header.php
+++ application/views/admin/templates/header.php
@@ -58,7 +58,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php"> <!-- <img alt="Charisma Logo" src="img/logo20.png" class="hidden-xs"/> -->
+            <a class="navbar-brand" href="<?php echo base_url(); ?>index.php/admin/adminindex"> <!-- <img alt="Charisma Logo" src="img/logo20.png" class="hidden-xs"/> -->
                 <span>Kamakshi Gifts</span></a>
 
             <!-- user dropdown starts -->
