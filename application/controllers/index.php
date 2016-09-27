e7577c32c92169dff8f3ecdc61aaf9fb34e372c4
thangamariappanp <thangamgold45@gmail.com>
2016-09-27 12:32:56 +0530

    Registeration Part changes

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -1,15 +1,16 @@
-<?php
-if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 session_start(); 
+
 class Index extends CI_Controller
 {
 public function __construct()
     {
         parent::__construct();
-        $this->load->model('register');
-        $this->load->helper('form');
+        $this->load->model('index_model');
         // Load form validation library
-        $this->load->library('form_validation');
+        $this->load->library(array('form_validation','session'));
+        $this->load->helper(array('url','html','form'));  
         
         // Load session library
         $this->load->library('session');
@@ -17,26 +18,22 @@ public function __construct()
     }
 	public function index()
 	{
-	  $data['giftstore_category'] = $this->register->get_register();
-      $data['giftstore_product'] = $this->register->get_latestproduct();
-	  // $data['giftstore_subcategory'] = $this->register->get_register();
-      $this->load->model('register');
-	  $this->load->view('index',$data);
-	}
-	
+    	$data['giftstore_category'] = $this->index_model->get_register();
+        $data['giftstore_product'] = $this->index_model->get_latestproduct();
+    	$this->load->view('index',$data);
+	}	
 	public function register()
 	{ 
-		$categories['giftstore_category'] = $this->register->get_register();
-		// $categories['giftstore_subcategory'] = $this->register->get_category();
+		$categories['giftstore_category'] = $this->index_model->get_register();
+		$categories['giftstore_subcategory'] = $this->index_model->get_category();
 		$this->load->view('register',$categories);
 
 	}
-
     public function category()
 	{
-		$categories['giftstore_category'] = $this->register->get_register();
-		$categories['gift_recipient'] = $this->register->get_recipient();
-		$category_values = $this->register->get_category();
+		$categories['giftstore_category'] = $this->index_model->get_register();
+		$categories['gift_recipient'] = $this->index_model->get_recipient();
+		$category_values = $this->index_model->get_category();
 		$categories['cat_name'] = $category_values['cat_name'];
 		$categories['gift_subcategory'] = $category_values['gift_subcategory'];
 		$categories['cat_pro_count'] = $category_values['cat_pro_count'];
@@ -49,11 +46,10 @@ public function __construct()
 			$this->load->view('no_products',$categories);
 		}
 	}
-
 	public function detail()
 	{
-		$categories['giftstore_category'] = $this->register->get_register();
-		$categories_values = $this->register->get_product_details();
+		$categories['giftstore_category'] = $this->index_model->get_register();
+		$categories_values = $this->index_model->get_product_details();
 		$categories['product_image_details'] = $categories_values['product_image_details'];
 		$categories['product_details'] = $categories_values['product_details'];
 		$categories['recommanded_products'] = $categories_values['recommanded_products'];
@@ -63,20 +59,10 @@ public function __construct()
 	}
 	public function contact()
 	{
-		$categories['giftstore_category'] = $this->register->get_register();
-		// $categories['giftstore_subcategory'] = $this->register->get_category();
+		$categories['giftstore_category'] = $this->index_model->get_register();
+		// $categories['giftstore_subcategory'] = $this->index_model->get_category();
 		$this->load->view('contact',$categories);
 	}
-
-	// public function reg_form()
- //    {
- //        $categories['giftstore_category']    = $this->register->get_register();
- //        $categories['giftstore_subcategory'] = $this->register->get_category();
- //        $this->register->get_reg_form();
-        
- //        $this->load->view('register', $categories);
- //    }
-
 	public function basket()
 	{
 		$this->load->view('basket');
@@ -99,139 +85,189 @@ public function __construct()
     }
     public function customer_account()
     {
-        $categories['giftstore_category']    = $this->register->get_register();
-        $categories['giftstore_subcategory'] = $this->register->get_category();
+        $categories['giftstore_category']    = $this->index_model->get_register();
+        $categories['giftstore_subcategory'] = $this->index_model->get_category();
         $this->load->view('customer_account', $categories);
     }
     public function customer_wishlist()
     {
-        $categories['giftstore_category']    = $this->register->get_register();
-        $categories['giftstore_subcategory'] = $this->register->get_category();
+        $categories['giftstore_category']    = $this->index_model->get_register();
+        $categories['giftstore_subcategory'] = $this->index_model->get_category();
         $this->load->view('customer_wishlist', $categories);
     }
     public function customer_orders()
     {
-        $categories['giftstore_category']    = $this->register->get_register();
-        $categories['giftstore_subcategory'] = $this->register->get_category();
+        $categories['giftstore_category']    = $this->index_model->get_register();
+        $categories['giftstore_subcategory'] = $this->index_model->get_category();
         $this->load->view('customer_orders', $categories);
     }
     public function customer_order()
     {
         $this->load->view('customer_order');
     }
+    //REGISTRATION FORM//
+    //SIGNUP//
     public function new_user_registration()
-    {
-        $categories['giftstore_category']    = $this->register->get_register();
-        // $categories['giftstore_subcategory'] = $this->register->get_category();
-        $this->load->view('register', $categories);
-        // Check validation for user input in SignUp form
-        $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
-        $this->form_validation->set_rules('email_reg', 'Email', 'trim|required|xss_clean');
-        $this->form_validation->set_rules('email_reg', 'Email', 'callback_rolekey_exists');
-        $this->form_validation->set_rules('password_reg', 'Password', 'trim|required|xss_clean');
-        if ($this->form_validation->run() == FALSE) {
-            $this->load->view('register');
-        } else {
-            $data   = array(
-                'user_name' => $this->input->post('username'),
-                'user_email' => $this->input->post('email_reg'),
-                'user_password' => md5($this->input->post('password_reg')),
-                'user_status' => '1'
-            );
-            $result = $this->register->registration_insert($data);
-            // $message = registration_insert();
-            // $data['message_display'] = $message;
-            // // echo $result;
-            //  if($result == FALSE){
-            //     $message="Email already exists";
-            //     $this->load->view('register', $data);
-            // }
-            //     elseif(){
-            //     $message="User already exists";
-            // }
-            //     else{
-            //     $message="registered successfully";
-            //     $this->load->view('register', $data);
-            //     }
-            //     return $message;
-        }
-    }
-    Public function rolekey_exists($key) 
-    {
-        $this->register->mail_exists($key);
-    }
-    // Check for user login process
-    public function user_login_process()
-    {
+    {   
+        // $categories['giftstore_category']    = $this->index_model->get_register();
+        // $categories['giftstore_subcategory'] = $this->index_model->get_category();
+        // $this->load->view('register', $categories);
+        // print_r($_POST);
+        $status = array();//array is initialized
+        $errors='';
+        $errors_array=array();
         $validation_rules = array(
+           array(
+                 'field'   => 'user_name',
+                 'label'   => 'User Name',
+                 'rules'   => 'trim|required|is_unique[giftstore_users.user_name]|xss_clean'
+              ),
+           array(
+                 'field'   => 'user_email',
+                 'label'   => 'User Email',
+                 'rules'   => 'trim|required|valid_email|is_unique[giftstore_users.user_email]|xss_clean'
+              ),
             array(
-                'field' => 'email',
-                'label' => 'Email',
-                'rules' => 'trim|required|xss_clean'
-            ),
-            array(
-                'field' => 'password',
-                'label' => 'Password',
-                'rules' => 'trim|required|xss_clean'
-            )
+                 'field'   => 'user_password',
+                 'label'   => 'User Password',
+                 'rules'   => 'trim|required|xss_clean'
+              )   
         );
-        $this->form_validation->set_rules($validation_rules); //through this statement rules are set
-        $errors_array = array(); //array is initialized
-        $empty_errors = ''; //array is initialized
-        
-        // $errors_array = $this->validation_errors_to_array($validation_rules);
+        $this->form_validation->set_rules($validation_rules);
+        // $this->form_validation->set_rules('user_email', 'Email', 'callback_rolekey_exists');
         if ($this->form_validation->run() == FALSE) {
-            foreach ($validation_rules as $row) {
-                $field = $row['field']; //getting field name
-                $error = form_error($field); //getting error for field name
-                //form_error() is inbuilt function
+            foreach($validation_rules as $row){
+                $field = $row['field'];          //getting field name
+                $error = form_error($field);    //getting error for field name
+                                                //form_error() is inbuilt function
                 //if error is their for field then only add in $errors_array array
-                 echo "error".$error;
-                if ($error) {
-                    if (strpos($error, "field is required.") !== false) {
-                        $empty_errors = $error;
+                // echo "error".$error;
+                if($error){
+                    if (strpos($error,"field is required.") !== false){
+                        $errors = $error; 
                         break;
-                    } else
-                        $errors_array[$field] = $error;
+                    }
+                    else{
+                        // echo "else";
+                        $errors = $error;
+                    }
                 }
             }
-            if (strpos($empty_errors, "field is required.") !== false) {
-                $data = array(
+            if (strpos($errors,"field is required.") !== false){  
+                 $status = array(
                     'error_message' => 'Please fill out all mandatory fields'
-                );
+                 );
             }
-            else if (array_key_exists("email",$errors_array) && strpos($errors_array['email'],"The username field must contain a valid email address.") !== false){
-                $data = array(
-                'error_message' => 'Please enter valid email address'
-                );
-            } 
-             // echo print_r($errors_array);
-            // if (isset($this->session->userdata['logged_in'])) {
-            //     $this->load->view('register');
-            // } else {
-            //     if (empty($errors_array) && $empty_errors == '')
-            //         $this->load->view('index');
-            //     else
-            //         $this->load->view('index', $data);
-            // }
-        } else {
-            $data   = array(
-                'user_name' => $this->input->post('username'),
-                'user_password' => $this->input->post('password')
-            );
+            else{
+                $status = array(
+                    'error_message' => $errors
+                 );
             }
-    } // Logout from admin page
-    public function logout()
-    {
-        
-        // Removing session data
-        $sess_array = array(
-            'username' => ''
-        );
-        $this->session->unset_userdata('logged_in', $sess_array);
-        $data['message_display'] = 'Successfully Logout';
-        $this->load->view('templates/header', $data);
+        }
+        else{
+            if(!empty($_POST)){
+                if (!empty($errors)) {
+                    $status = array(
+                        'error_message' => strip_tags($errors)
+                    );
+                }
+                else{
+                    $data = array(
+                    'user_name' => $this->input->post('user_name'),
+                    'user_email' => $this->input->post('user_email'),
+                    'user_password' => md5($this->input->post('user_password'))
+                    );
+                    $result = $this->index_model->insert_user_registration($data);
+                    if($result)
+                        $status = array(
+                            'error_message' => "Registration Success!"
+                    );
+                }       
+            }
+        }
+        // print_r($status);    
+        $this->load->view('register',$status);
     }
+//SIGNUP END//
+//LOGIN FORM START//
+    public function user_login_process()
+    {   
+        // $categories['giftstore_category']    = $this->index_model->get_register();
+        // $categories['giftstore_subcategory'] = $this->index_model->get_category();
+        // $this->load->view('header', $categories);
+        // print_r($_POST);
+        $status = array();//array is initialized
+        $errors='';
+        $errors_array=array();
+        $validation_rules = array(
+           array(
+                 'field'   => 'user_email',
+                 'label'   => 'User Email',
+                 'rules'   => 'trim|required|valid_email|is_unique[giftstore_users.user_email]|xss_clean'
+              ),
+            array(
+                 'field'   => 'user_password',
+                 'label'   => 'User Password',
+                 'rules'   => 'trim|required|xss_clean'
+              )   
+        );
+        $this->form_validation->set_rules($validation_rules);
+        // $this->form_validation->set_rules('user_email', 'Email', 'callback_rolekey_exists');
+        if ($this->form_validation->run() == FALSE) {
+            foreach($validation_rules as $row){
+                $field = $row['field'];          //getting field name
+                $error = form_error($field);    //getting error for field name
+                                                //form_error() is inbuilt function
+                //if error is their for field then only add in $errors_array array
+                // echo "error".$error;
+                if($error){
+                    if (strpos($error,"field is required.") !== false){
+                        $errors = $error; 
+                        break;
+                    }
+                    else{
+                        // echo "else";
+                        $errors = $error;
+                    }
+                }
+            }
+            if (strpos($errors,"field is required.") !== false){  
+                 $status = array(
+                    'error_message_login' => 'Please fill out all mandatory fields'
+                 );
+            }
+            else{
+                $status = array(
+                    'error_message_login' => $errors
+                 );
+            }
+        }
+        else{
+            if(!empty($_POST)){
+                if (!empty($errors)) {
+                    $status = array(
+                        'error_message_login' => strip_tags($errors)
+                    );
+                }
+                else{
+                    $data = array(
+                    'user_email' => $this->input->post('user_email'),
+                    'user_password' => md5($this->input->post('user_password'))
+                    );
+                    $result = $this->index_model->insert_user_registration($data);
+                    if($result)
+                        $status = array(
+                            'error_message_login' => "Login Success!"
+                    );
+                }       
+            }
+        }
+        // print_r($status);    
+        $this->load->view('register',$status);
+    }
+
+        
 }
+
 /* End of file welcome.php */
 /* Location: ./application/controllers/welcome.php */
--- application/models/index_model.php
+++ application/models/index_model.php
@@ -1,12 +1,10 @@
 <?php
-class Register extends CI_Model
+class Index_Model extends CI_Model
 {
-    
     public function __construct()
     {
         $this->load->database();
-    }
-    
+    }    
     public function get_register()
     {
         $query = $this->db->get('giftstore_category');
@@ -36,30 +34,44 @@ class Register extends CI_Model
         }
         return $query;
     }
-    
-    public function registration_insert($data)
-    {
-        
-        // Query to check whether username already exist or not
-        $where = '(user_name="' . $data['user_name'] . '" or user_email="' . $data['user_email'] . '")';
+    //REGISTRATION FORM//
+    //SIGNUP MODEL//
+    public function insert_user_registration($data)
+    {   
+        // Query to check whether user name/email already exist or not
+        $condition = "user_name =" . "'" . $data['user_name'] . "'";
         $this->db->select('*');
         $this->db->from('giftstore_users');
-        $this->db->where($where);
+        $this->db->where($condition);
+        // $this->db->limit(1);
         $query = $this->db->get();
-        if ($query->num_rows() > 0) {
+        if ($query->num_rows() == 0) {
+            // Query to insert data in database
+            $this->db->insert('giftstore_users', $data);
+            if ($this->db->affected_rows() > 0) {
+                return true;
+            }
+        } else {
             return false;
         }
-        return $query;
     }
-    public function mail_exists($key)
-    {
-        $this->db->where('user_email', $key);
-        $query = $this->db->get('giftstore_users');
-        if ($query->num_rows() > 0) {
-            // print_r("Email already exists");
-            return true;
+    //LOGIN MODEL//
+    public function insert_user_login($data)
+    {   
+        // Query to check whether login name already exist or not
+        $condition = "user_email =" . "'" . $data['user_email'] . "'";
+        $this->db->select('*');
+        $this->db->from('giftstore_users');
+        $this->db->where($condition);
+        // $this->db->limit(1);
+        $query = $this->db->get();
+        if ($query->num_rows() == 0) {
+            // Query to insert data in database
+            $this->db->insert('giftstore_users', $data);
+            if ($this->db->affected_rows() > 0) {
+                return true;
+            }
         } else {
-            // print_r("Email registered");
             return false;
         }
     }
@@ -86,10 +98,7 @@ class Register extends CI_Model
             $query['product_category'] = $cat_product->get()->result_array();
             $query['cat_pro_count']    = count($query['product_category']);
         }
-        
-        
-        
-        return $query;
+        // return $query;
     }
     
     public function get_product_details()
@@ -154,38 +163,5 @@ class Register extends CI_Model
         return $query;
     }
     
-    // Read data from database to show data in admin page
-    public function read_user_information($username)
-    {
-        
-        $condition = "user_name =" . "'" . $username . "'";
-        $this->db->select('*');
-        $this->db->from('giftstore_users');
-        $this->db->where($condition);
-        $this->db->limit(1);
-        $query = $this->db->get();
-        
-        if ($query->num_rows() == 1) {
-            return $query->result();
-        } else {
-            return false;
-        }
-    }
-    
-    // Read data using username and password
-    public function login($data)
-    {
-        $condition = "user_name =" . "'" . $data['username'] . "' AND " . "user_password =" . "'" . $data['password'] . "'";
-        $this->db->select('*');
-        $this->db->from('giftstore_users');
-        $this->db->where($condition);
-        $this->db->limit(1);
-        $query = $this->db->get();
-        
-        if ($query->num_rows() == 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }   
+       
 }
