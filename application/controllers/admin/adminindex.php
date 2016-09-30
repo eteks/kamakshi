8a863826ad94abd77a7393c15c72366c1d18d866
thangamariappanp <thangamgold45@gmail.com>
2016-09-30 12:00:16 +0530

    changes over city based location

--- application/controllers/admin/adminindex.php
+++ application/controllers/admin/adminindex.php
@@ -871,12 +871,94 @@ class Adminindex extends CI_Controller {
 		$this->load->view('admin/edit_endusers');
 	}
 	public function area()
-	{	
-		$this->load->view('admin/area');
+	{
+		$area['area_list'] = $this->location->get_area();	
+		$this->load->view('admin/area',$area);
 	}
 	public function add_area()
 	{	
-		$this->load->view('admin/add_area');
+		$status = array();//array is initialized
+		$errors='';
+		$validation_rules = array(
+	       array(
+	             'field'   => 'state_name',
+	             'label'   => 'State',
+	             'rules'   => 'trim|required|xss_clean'
+	          ),
+	        array(
+	             'field'   => 'city_name',
+	             'label'   => 'City',
+	             'rules'   => 'trim|required|xss_clean'
+	          ),
+	        array(
+	             'field'   => 'area_name',
+	             'label'   => 'Area',
+	             'rules'   => 'trim|required|xss_clean'
+	          ),
+	        array(
+	             'field'   => 'area_delivery_charge',
+	             'label'   => 'Area',
+	             'rules'   => 'trim|required|xss_clean'
+	          ),
+	       array(
+	             'field'   => 'city_status',
+	             'label'   => 'Status',
+	             'rules'   => 'trim|required|xss_clean'
+	          ),      
+	    );
+	    $this->form_validation->set_rules($validation_rules);
+	    if ($this->form_validation->run() == FALSE) {
+	    	foreach($validation_rules as $row){
+	            $field = $row['field'];          //getting field name
+	            $error = form_error($field);    //getting error for field name
+	                                            //form_error() is inbuilt function
+	            //if error is their for field then only add in $errors_array array
+	            // echo "error".$error;
+	            if($error){
+	                if (strpos($error,"field is required.") !== false){
+	                    $errors = $error; 
+	                    break;
+	                }
+	                else
+	                    $errors[$field] = $error; 
+	            }
+        	}
+	        if (strpos($errors,"field is required.") !== false){  
+	             $status = array(
+	                'error_message' => 'Please fill out all mandatory fields'
+	             );
+	        }
+    	}
+    	else {
+    		if(!empty($_POST)) {
+				if (!empty($errors)) {
+					$status = array(
+	                	'error_message' => strip_tags($errors)
+	             	);
+				}
+				else{
+					$data = array(
+						'area_name' => $this->input->post('area_name'),
+						'area_delivery_charge' => $this->input->post('area_delivery_charge'),
+						'city_id' => $this->input->post('city_name'),
+						'city_state_id' => $this->input->post('state_name'),
+						'city_status' => $this->input->post('city_status'),
+					);
+					$result = $this->location->insert_city($data);
+					if($result)
+						$status = array(
+	                		'error_message' => "Area Inserted Successfully!"
+	             		);
+					else
+						$status = array(
+	                		'error_message' => "Area Already Exists!"
+	             		);
+				}		
+			}
+    	}
+		// print_r($status);	
+		$status['area_list'] = $this->location->get_area();
+		$this->load->view('admin/add_area',$status);
 	}
 	public function edit_area()
 	{	
@@ -884,6 +966,7 @@ class Adminindex extends CI_Controller {
 	}
 	public function city()
 	{	
+		$city['city'] = $this->location->get_cities();
 		$city['city_list'] = $this->location->get_city();
 		$this->load->view('admin/city',$city);
 	}
@@ -973,6 +1056,11 @@ class Adminindex extends CI_Controller {
 			$status = '';//array is initialized
 			$errors = '';
 			$validation_rules = array(
+				array(
+		             'field'   => 'state_id',
+		             'label'   => 'State',
+		             'rules'   => 'trim|required|xss_clean'
+		          ),
 		       array(
 		             'field'   => 'edit_city_name',
 		             'label'   => 'City',
@@ -1005,22 +1093,27 @@ class Adminindex extends CI_Controller {
 		             $status = 'Please fill out all mandatory fields';
 		        }
     		}
-			else{
+    		else{
+				if (!empty($errors)) {
+					$status = strip_tags($errors);
+				}
+				else{
 					$data = array(
 					'city_id' => $id,
 					'city_name' => $this->input->post('edit_city_name'),
+					'state_name' => $this->input->post('city_state_id'),
 					'city_status' => $this->input->post('edit_city_status'),
 					);
-					$result = $this->location->update_city($data);
+					$result = $this->catalog->update_product_attribute($data);
 					if($result)
 						$status = "City Updated Successfully!";
 					else
 						$status = "City Already Exists!";
-				}
+				}		
+    		}
     		$data['status'] = $status;
 		}
 		$data['city_data'] = $this->location->get_city_data($id);
-		// print_r($data);
 		$this->load->view('admin/edit_city',$data);
 	}
 	public function state()
