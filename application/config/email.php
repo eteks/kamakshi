71030df30849a45022a152764164868b824ae9c8
kalaiR <kalamca.gs@gmail.com>
2016-12-01 18:46:26 +0530

    update forgot password email sending issues fixed

--- application/config/email.php
+++ application/config/email.php
@@ -1,10 +1,10 @@
 <?php
 $config = Array(
     'protocol' => 'smtp',
-    'smtp_host' => 'ssl://smtp.googlemail.com',
+    'smtp_host' => 'ssl://smtp.gmail.com',
     'smtp_port' => 465,
-    'smtp_user' => 'dspmuthukaruppan@gmail.com',
-    'smtp_pass' => '921811104303',
+    'smtp_user' => 'thangamgold45@gmail.com',
+    'smtp_pass' => '',
     'mailtype'  => 'html', 
     'charset'   => 'iso-8859-1',
     'wordwrap'  => TRUE,
